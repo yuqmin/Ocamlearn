0b527d3a7858370dc7a95af1be95eb8129e0fb9a
yuqmin <167790099+yuqmin@users.noreply.github.com>
2025-06-21 01:35:31 +0000

    test

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,4 @@
+let () = Random.self_init ()
 let x = Random.int 9 + 1 (* not 0 *);;
 
 let () = Printf.printf "x = %d\n" x
---
+++ test/test1.ml
@@ -0,0 +1,2 @@
+let y = Random.int 9 + 2 (* not 0 or 1 *)
+let () = Printf.printf "y = %d\n" y
